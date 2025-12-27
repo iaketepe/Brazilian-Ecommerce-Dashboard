f59fbefc93cb886d77b0f3954f705e776a83e2ad
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-12-26 19:00:03 -0500

    Simplified runner by moving db manipulation to storage module

--- pipeline/main.py
+++ pipeline/main.py
@@ -9,8 +9,8 @@ if not(db.is_connected()):
     sys.exit(1)
 
 
-if not(db.select_exists_schema("TEST_ACT1")):
-    print("Db exists but no data")
+if not(db.select_exists_schema("TEST_ACT1") and db.select_exists_schema("TEST_ACT2")):
+    print("Db exists but incomplete data")
     r = runner.Runner(db)
     r.start()
 else:
--- pipeline/runner.py
+++ pipeline/runner.py
@@ -1,30 +1,14 @@
-from pipeline.running import processor
+from pipeline.running import storage
+# from pipeline.running import processor
 
 
 class Runner:
     def __init__(self, db):
         self.db = db
 
-    # def createDBAct(schema_name, schema array)
-        #
-
     def start(self):
         try:
-            # storage.store(ACT1)->ACT2->ACT3
-            # if there is no data create the data myself
-            schema = "TEST_ACT1"
-            tables = ["metrics, order_status, cumulative_revenue"]
-            # create schema
-            self.db.create_schema(schema)
-            # create tables
-            for table in tables:
-                self.db.create_table(schema, table)
-            # write to table
-            if self.db.select_exists("TEST_ACT1","metrics"):
-                self.db.write_to_table("TEST_ACT1","metrics",processor.acts["ACT1"]["metrics"])
-            if self.db.select_exists("TEST_ACT1","order_status"):
-                self.db.write_to_table("TEST_ACT1","order_status",processor.acts["ACT1"]["order_status"])
-            if self.db.select_exists("TEST_ACT1","cumulative_revenue"):
-                self.db.write_to_table("TEST_ACT1","cumulative_revenue",processor.acts["ACT1"]["cumulative_revenue"])
+            # storage.store(TEST,ACT1)->ACT2->ACT3
+            storage.store(self.db, "TEST", "ACT1")
         except Exception as e:
             print(e)
