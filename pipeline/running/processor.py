0c1ddd7750458f734553e8ab09696503a67b6028
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-03-16 16:44:52 -0400

    amended changes in processor module

--- pipeline/running/processor.py
+++ pipeline/running/processor.py
@@ -1,13 +1,12 @@
-from pipeline.running import ingestion, act_processes
-from act_processes import a1proc, a2proc, a3proc
-import pandas as pd
+from pipeline.running import ingestion
+from resources.acts import a1, a2, a3
 
 dfs = ingestion.ingest()
 
 acts = {}
 
-acts["ACT1"] = a1proc.calculate(dfs)
-acts["ACT2"] = a2proc.calculate(dfs)
-acts["ACT3"] = a3proc.calculate(dfs)
+acts["ACT1"] = a1.proc.calculate(dfs)
+acts["ACT2"] = a2.proc.calculate(dfs)
+acts["ACT3"] = a3.proc.calculate(dfs)
 # acts["ACT2"] = a2proc.calculate(dfs)
 # acts["ACT3"] = a3proc.calculate(dfs)
