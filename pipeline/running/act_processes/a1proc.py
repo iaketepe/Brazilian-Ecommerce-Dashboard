c2a00d438a82f65bd5a3a4845b05ba16d812784c
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-03-16 15:57:32 -0400

    Moved Act1 one processes to act1proc

---
+++ pipeline/running/act_processes/a1proc.py
@@ -0,0 +1,124 @@
+
+import pandas as pd
+# ACT 1:
+
+def calculate(dfs):
+    # 0: BIG NUMBER - Approximate Annual Revenue
+    delivered_orders = dfs['olist_orders_dataset'][dfs['olist_orders_dataset']['order_status'] == 'delivered']
+
+    delivered_orders = delivered_orders[delivered_orders['order_delivered_customer_date'].notna()]
+
+    # take just the columns you need
+    df_payments = dfs['olist_order_payments_dataset'][['order_id', 'payment_value']]
+
+    # group by order_id and sum the payments
+    df_revenue = df_payments.groupby('order_id', as_index=False).sum()
+
+    df_delivered_revenue = df_revenue.merge(
+        delivered_orders[['order_id','order_delivered_customer_date']],
+        on='order_id',
+        how='inner'
+    )
+
+    total_verified_revenue = df_delivered_revenue['payment_value'].sum()
+
+    # 1: Cumulative Approximate Annual Revenue
+
+    df_month_delivered_revenue = df_delivered_revenue.copy()
+    df_month_delivered_revenue['order_delivered_customer_date'] = pd.to_datetime(df_month_delivered_revenue['order_delivered_customer_date'])
+    df_month_delivered_revenue = df_month_delivered_revenue.groupby(
+        df_month_delivered_revenue['order_delivered_customer_date'].dt.to_period('M')
+    )['payment_value'].sum().reset_index(name='monthly_revenue')
+
+    df_month_delivered_revenue['order_delivered_customer_date'] = df_month_delivered_revenue['order_delivered_customer_date'].astype(str)
+
+    df_month_delivered_revenue['cumulative_revenue'] = df_month_delivered_revenue['monthly_revenue'].cumsum()
+
+    month_delivered_revenue = df_month_delivered_revenue.to_dict(orient='records')
+
+    _ = """df_delivered_revenue = df_delivered_revenue.sort_values('order_delivered_customer_date')
+    
+    df_delivered_revenue['cumulative_revenue'] = df_delivered_revenue['payment_value'].cumsum()"""
+
+    # 2: review scores overall average
+    review_score_avg = dfs['olist_order_reviews_dataset']['review_score'].mean()
+    review_score_max = dfs['olist_order_reviews_dataset']['review_score'].max()
+
+    # 3 Ratio of Orders shipped from seller to carrier before deadline:
+
+    delivered_orders_carrier_dates = delivered_orders[['order_id','order_delivered_carrier_date']]
+
+    seller_shipping_limits = dfs['olist_order_items_dataset'][['order_id','shipping_limit_date']].copy()
+    seller_shipping_limits = seller_shipping_limits.drop_duplicates(subset='order_id', keep='last')
+
+    orders_seller_carrier = delivered_orders_carrier_dates.copy()
+    orders_seller_carrier = delivered_orders_carrier_dates.merge(
+        seller_shipping_limits,
+        on='order_id',
+        how='inner'  # only keep orders that exist in both
+    )
+
+    orders_delivered_shipped_sum = (orders_seller_carrier['order_delivered_carrier_date'] <= orders_seller_carrier['shipping_limit_date']).sum()
+    ratio_orders_delivered_shipped = orders_delivered_shipped_sum / orders_seller_carrier.shape[0]
+
+
+    # 4 Ratio of Orders paid in full with installments
+
+    df_payment_installments = df_delivered_revenue.copy()
+
+    df_payment_installments = df_payment_installments.merge(
+        dfs['olist_order_items_dataset'][['order_id','price']],
+        on='order_id',
+        how='inner'
+    )
+
+    delivered_orders_paid_in_full_sum = (df_payment_installments['payment_value'] >= df_payment_installments['price']).sum()
+    ratio_delivered_orders_paid_in_full = delivered_orders_paid_in_full_sum / df_payment_installments.shape[0]
+
+    # 5 Ratio of Orders delivered to customer before or on deadline:
+
+    orders_estimated_delivered_sum = (delivered_orders['order_delivered_customer_date'] <= delivered_orders['order_estimated_delivery_date']).sum()
+    ratio_orders_estimated_delivered = orders_estimated_delivered_sum / delivered_orders.shape[0]
+
+    # 6 Distribution of Orders by Status
+
+    return {
+            "metrics": [
+                {
+                    "name": "total_verified_revenue",
+                    "description": "Total approximate annual revenue from delivered orders",
+                    "value": str(total_verified_revenue)
+                },
+                {
+                    "name": "review_score_avg",
+                    "description": "Average review score of all orders",
+                    "value": str(review_score_avg)
+                },
+                {
+                    "name": "review_score_max",
+                    "description": "Maximum review score of all orders",
+                    "value": str(review_score_max)
+                },
+                {
+                    "name": "ratio_orders_delivered_shipped",
+                    "description": "Ratio of orders shipped before shipping limit date",
+                    "value": str(ratio_orders_delivered_shipped)
+                },
+                {
+                    "name": "ratio_delivered_orders_paid_in_full",
+                    "description": "Ratio of delivered orders paid in full",
+                    "value": str(ratio_delivered_orders_paid_in_full)
+                },
+                {
+                    "name": "ratio_orders_estimated_delivered",
+                    "description": "Ratio of orders delivered on or before estimated delivery date",
+                    "value": str(ratio_orders_estimated_delivered)
+                },
+            ],
+            "cumulative_revenue": month_delivered_revenue, #df_delivered_revenue[['order_id', 'order_delivered_customer_date', 'cumulative_revenue']].to_dict(orient='records'),
+            "order_status": [
+                dfs['olist_orders_dataset']['order_status']
+                .value_counts()
+                .to_dict()
+            ]
+        }
