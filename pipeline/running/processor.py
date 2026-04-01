d135389f99335ce2c77f024f345795619b86ad63
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-04-01 19:41:28 -0400

    Removed implicit importing of proc and viz

--- pipeline/running/processor.py
+++ pipeline/running/processor.py
@@ -1,12 +1,15 @@
 from pipeline.running import ingestion
-from resources.acts import a1, a2, a3, a4
+from resources.acts.a1 import proc as a1_proc
+from resources.acts.a2 import proc as a2_proc
+from resources.acts.a3 import proc as a3_proc
+from resources.acts.a4 import proc as a4_proc
 
 dfs = ingestion.ingest()
 
 acts = {}
 
-acts["ACT1"] = a1.proc.calculate(dfs)
-acts["ACT2"] = a2.proc.calculate(dfs)
-acts["ACT3"] = a3.proc.calculate(dfs)
-acts["ACT4"] = a4.proc.calculate(dfs)
+acts["ACT1"] = a1_proc.calculate(dfs)
+acts["ACT2"] = a2_proc.calculate(dfs)
+acts["ACT3"] = a3_proc.calculate(dfs)
+acts["ACT4"] = a4_proc.calculate(dfs)
 # acts["ACT3"] = a3proc.calculate(dfs)
