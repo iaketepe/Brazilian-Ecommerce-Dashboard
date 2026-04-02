a583fdb3ed6d94c05979f5c02722fb7d2f5bd84f
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-04-02 14:13:22 -0400

    Converted Pipeline main file ot prefect-compatible version

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,19 +1,33 @@
 from pipeline import runner
 from pipeline.db import DB
+from prefect import flow, task
 import sys
 
-db = DB()
+@task
+def setup_db():
+    db = DB()
 
-if not(db.is_connected()):
-    print("!DB -> Ending Prog...")
-    sys.exit(1)
+    if not(db.is_connected()):
+        print("!DB -> Ending Prog...")
+        sys.exit(1)
+    return db
 
-try:
-    r = runner.Runner(db)
-    r.start()
-except Exception as e:
-    print(e)
-    sys.exit(1)
+@task
+def execute_runner(db):
+    try:
+        r = runner.Runner(db)
+        r.start()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+@flow
+def main():
+    db = setup_db()
+    execute_runner(db)
+
+if __name__ == "__main__":
+    main()
 
 #if not(db.select_exists_schema("TEST_ACT1") and db.select_exists_schema("TEST_ACT2")):
     #print("Db exists but incomplete data")
