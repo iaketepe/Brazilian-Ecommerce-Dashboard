49dd2da11659d56c58a77bc6f4ab41a229ef1d1c
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-04-05 14:18:32 -0400

    Removed redundant parentheses in pipeline.running storage store method

--- pipeline/running/storage.py
+++ pipeline/running/storage.py
@@ -11,5 +11,5 @@ def store(db, schema_base, act_name):
     for table_name in table_names:
         sqltypes = sqlizer.get_sql_types(act[table_name])
         db.create_table(schema_name, table_name, sqltypes)
-        if (not(db.data_exists(schema_name,table_name))):
+        if not(db.data_exists(schema_name,table_name)):
             db.write_to_table(schema_name, table_name, act[table_name])
