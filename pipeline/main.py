40d022dda519abc5a46c14e91a063f8e3e2556fd
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-04-05 13:10:30 -0400

    Simplified pipeline.main for better readability and workflow handling

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,24 +1,9 @@
 from pipeline import runner
 from pipeline.db import DB
-import sys
 
-db = DB()
-
-if not(db.is_connected()):
-    print("!DB -> Ending Prog...")
-    sys.exit(1)
-
-try:
+def main():
+    db = DB()
     r = runner.Runner(db)
-    status = r.start()
-    if status == 'FAILURE':
-        Exception("Pipeline Failure")
-except Exception as e:
-    print(e)
-    sys.exit(1)
+    r.start()
 
-#if not(db.select_exists_schema("TEST_ACT1") and db.select_exists_schema("TEST_ACT2")):
-    #print("Db exists but incomplete data")
-
-#else:
-    #print("DB exists")
+main()
