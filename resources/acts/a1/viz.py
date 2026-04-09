97de138ca280348efe6f45e58d7fd73762aff3de
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2026-04-09 19:26:47 -0400

    Removed delta on number figures for better UX

--- resources/acts/a1/viz.py
+++ resources/acts/a1/viz.py
@@ -12,7 +12,7 @@ class Act1:
 
     def annual_revenue_approximated(self):
         fig = go.Figure(go.Indicator(
-            mode="number+delta",
+            mode="number",
             value=float(self.metrics.total_verified_revenue),
             title={"text": "Approximate Total Revenue"},
         ))
@@ -33,7 +33,7 @@ class Act1:
 
     def review_score(self):
         fig = go.Figure(go.Indicator(
-            mode="number+delta",
+            mode="number",
             value=float(self.metrics.review_score_avg),
             title={"text": "Average Review Score"},
         ))
